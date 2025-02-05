f68b0da0cb3e33d99353a9defd797e028628b626
HanadAlly <allyhanad404@gmail.com>
2025-02-05 18:46:35 +0300

    commit changes

--- js/contact-us.js
+++ js/contact-us.js
@@ -2,8 +2,7 @@
     event.preventDefault();
 
     const phoneNumber = "+254114235857";
-
-    // Display phone number and create a link to dial it
+    
     const output = document.getElementById("output");
     output.innerHTML = `To contact us, click to call: <a href="tel:${phoneNumber}">${phoneNumber}</a>`;
   }
