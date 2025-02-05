6a0926962f6294d6eb7d8737618d4712fc09ea3d
HanadAlly <allyhanad404@gmail.com>
2025-02-05 21:05:07 +0300

    commit changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
- // Function to handle form submission and scroll to the phone number
+ 
 function handleSubmit(event) {
-    event.preventDefault(); // Prevent the form from submitting
+    event.preventDefault();
 
     
     document.getElementById("phone-section").scrollIntoView({ behavior: 'smooth' });
